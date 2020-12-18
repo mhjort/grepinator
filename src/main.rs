1719372fb6840e60f498e2caa6f0c88fbfd86aab
Markus Hjort <markus.hjort@gmail.com>
2020-12-18 18:16:44 +0200

    Improve error message

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let (query, filename) = parse_config(&args);
 
-    let contents = fs::read_to_string(filename).expect("Failed to read file");
+    let contents = fs::read_to_string(filename).expect(&format!("Failed to read file {}", filename));
     let matches = search(query, &contents);
     for matched_line in matches {
         println!("{}", matched_line);
